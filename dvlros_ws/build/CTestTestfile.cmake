e05cd636d5782b27d50b5e8429720c4a99abbc15
brucdeng <122509493+brucdeng@users.noreply.github.com>
2024-07-21 15:37:55 -0700

    Delete dvlros_ws directory

--- dvlros_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bd/dvlros_ws/src
-# Build directory: /home/bd/dvlros_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("test_ros")
--- dvlros_ws/build/test_ros/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bd/dvlros_ws/src/test_ros
-# Build directory: /home/bd/dvlros_ws/build/test_ros
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
